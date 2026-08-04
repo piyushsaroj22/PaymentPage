6c5f02c6141c9af9548b543f5dab83d437ba5591
piyush saroj <piyushsaroj222006@gmail.com>
2026-08-04 19:12:22 +0530

    feat: setup React frontend

--- frontend/src/app.tsx
+++ frontend/src/app.tsx
@@ -1,40 +1,40 @@
-import { useState } from 'preact/hooks'
-import preactLogo from './assets/preact.svg'
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
-import './app.css'
+import './App.css'
 
-export function App() {
+function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
       <section id="center">
-        <div class="hero">
-          <img src={heroImg} class="base" width="170" height="179" alt="" />
-          <img src={preactLogo} class="framework" alt="Preact logo" />
-          <img src={viteLogo} class="vite" alt="Vite logo" />
+        <div className="hero">
+          <img src={heroImg} className="base" width="170" height="179" alt="" />
+          <img src={reactLogo} className="framework" alt="React logo" />
+          <img src={viteLogo} className="vite" alt="Vite logo" />
         </div>
         <div>
           <h1>Get started</h1>
           <p>
-            Edit <code>src/app.tsx</code> and save to test <code>HMR</code>
+            Edit <code>src/App.tsx</code> and save to test <code>HMR</code>
           </p>
         </div>
         <button
           type="button"
-          class="counter"
+          className="counter"
           onClick={() => setCount((count) => count + 1)}
         >
           Count is {count}
         </button>
       </section>
 
-      <div class="ticks"></div>
+      <div className="ticks"></div>
 
       <section id="next-steps">
         <div id="docs">
-          <svg class="icon" role="presentation" aria-hidden="true">
+          <svg className="icon" role="presentation" aria-hidden="true">
             <use href="/icons.svg#documentation-icon"></use>
           </svg>
           <h2>Documentation</h2>
@@ -42,20 +42,20 @@ export function App() {
           <ul>
             <li>
               <a href="https://vite.dev/" target="_blank">
-                <img class="logo" src={viteLogo} alt="" />
+                <img className="logo" src={viteLogo} alt="" />
                 Explore Vite
               </a>
             </li>
             <li>
-              <a href="https://preactjs.com/" target="_blank">
-                <img class="button-icon" src={preactLogo} alt="" />
+              <a href="https://react.dev/" target="_blank">
+                <img className="button-icon" src={reactLogo} alt="" />
                 Learn more
               </a>
             </li>
           </ul>
         </div>
         <div id="social">
-          <svg class="icon" role="presentation" aria-hidden="true">
+          <svg className="icon" role="presentation" aria-hidden="true">
             <use href="/icons.svg#social-icon"></use>
           </svg>
           <h2>Connect with us</h2>
@@ -63,7 +63,11 @@ export function App() {
           <ul>
             <li>
               <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg class="button-icon" role="presentation" aria-hidden="true">
+                <svg
+                  className="button-icon"
+                  role="presentation"
+                  aria-hidden="true"
+                >
                   <use href="/icons.svg#github-icon"></use>
                 </svg>
                 GitHub
@@ -71,7 +75,11 @@ export function App() {
             </li>
             <li>
               <a href="https://chat.vite.dev/" target="_blank">
-                <svg class="button-icon" role="presentation" aria-hidden="true">
+                <svg
+                  className="button-icon"
+                  role="presentation"
+                  aria-hidden="true"
+                >
                   <use href="/icons.svg#discord-icon"></use>
                 </svg>
                 Discord
@@ -79,7 +87,11 @@ export function App() {
             </li>
             <li>
               <a href="https://x.com/vite_js" target="_blank">
-                <svg class="button-icon" role="presentation" aria-hidden="true">
+                <svg
+                  className="button-icon"
+                  role="presentation"
+                  aria-hidden="true"
+                >
                   <use href="/icons.svg#x-icon"></use>
                 </svg>
                 X.com
@@ -87,7 +99,11 @@ export function App() {
             </li>
             <li>
               <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg class="button-icon" role="presentation" aria-hidden="true">
+                <svg
+                  className="button-icon"
+                  role="presentation"
+                  aria-hidden="true"
+                >
                   <use href="/icons.svg#bluesky-icon"></use>
                 </svg>
                 Bluesky
@@ -97,8 +113,10 @@ export function App() {
         </div>
       </section>
 
-      <div class="ticks"></div>
+      <div className="ticks"></div>
       <section id="spacer"></section>
     </>
   )
 }
+
+export default App
--- frontend/src/services/payment.service.ts
+++
@@ -1,9 +0,0 @@
-import api from "./api";
-
-export const createOrder = async (amount: number) => {
-  const response = await api.post("/payment/create-order", {
-    amount,
-  });
-
-  return response.data;
-};
